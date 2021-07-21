35ace8a5d41d91cf6031f00e8a60da2d2f0253a8
Hugo Costa <hugo.santos.costa@gmail.com>
2021-07-21 17:17:29 +0100

    literally one line deleted

--- octoprint_BLOCKS/__init__.py
+++ octoprint_BLOCKS/__init__.py
@@ -116,5 +116,5 @@ def __plugin_load__():
     global __plugin_hooks__
     __plugin_hooks__ = {
         "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information,
-        
+
     }
