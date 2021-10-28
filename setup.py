55e59b498cb840732e109dfd36423e33f8655935
Hugo Costa <hugo.santos.costa@gmail.com>
2021-10-28 11:56:36 +0100

    name changes

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ plugin_package = "octoprint_BLOCKS"
 
 # The plugin's human readable name. Can be overwritten within OctoPrint's internal data via __plugin_name__ in the
 # plugin module
-plugin_name = "Blocks"
+plugin_name = "OctoPrint-BLOCKS"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
 plugin_version = "1.0.0"
@@ -59,7 +59,7 @@ plugin_ignored_packages = []
 # Example:
 #     plugin_requires = ["someDependency==dev"]
 #     additional_setup_parameters = {"dependency_links": ["https://github.com/someUser/someRepo/archive/master.zip#egg=someDependency-dev"]}
-additional_setup_parameters = {}
+additional_setup_parameters = {"python_requires": ">=3.3, <4"}
 
 ########################################################################################################################
 
